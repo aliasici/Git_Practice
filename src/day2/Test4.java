d32e3d773b7f4fcbc978f917186e7e482e38529c
aliasici <asiciali@yahoo.com>
2020-08-20 13:44:26 -0700

    Test4.java

--- src/day2/Test4.java
+++ src/day2/Test4.java
@@ -3,5 +3,6 @@ package day2;
 public class Test4 {
     public static void main(String[] args) {
         System.out.println("I love coding");
+        System.out.println("I do love Jva");
     }
 }
